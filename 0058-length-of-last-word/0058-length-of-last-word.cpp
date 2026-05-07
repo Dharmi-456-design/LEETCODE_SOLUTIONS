19f90ad939c712d7aa8882ed620c6a1927541e0b
Dharmi Patel <dharmi.patel.cg@gmail.com>
2026-05-07 11:41:47 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.01%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size(), len = 0;
+
+        int i = n - 1;
+        while(i >= 0 && s[i] == ' ') i--;
+        while( i >= 0 && s[i] != ' '){
+        len++;
+        i--;
+        }
+        return len;
+    }
+};
