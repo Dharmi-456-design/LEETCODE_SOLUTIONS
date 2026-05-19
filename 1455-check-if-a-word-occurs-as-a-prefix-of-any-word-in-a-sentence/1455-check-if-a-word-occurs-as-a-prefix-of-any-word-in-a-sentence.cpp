4d2ca898c87f8b43d880918404a8ee5b86ac806c
Dharmi Patel <dharmi.patel.cg@gmail.com>
2026-05-19 21:06:01 +0530

    Time: 0 ms (100%), Space: 8.3 MB (25.12%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        string word ="";
+        int index = 1;
+
+        for(int i =0; i <= sentence.size(); i++){
+            if(i == sentence.size() || sentence[i] == ' '){
+                if(word.find(searchWord) == 0){
+                    return index;
+                }
+                word = "";
+                index++;
+
+            }
+            else {
+                word += sentence[i];
+            }
+        }
+        return -1;
+    }
+};
