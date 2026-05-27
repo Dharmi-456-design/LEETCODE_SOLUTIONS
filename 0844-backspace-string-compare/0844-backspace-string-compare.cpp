19694f8d8a5ded35bd51f6c14a3646eb3078afb7
Dharmi Patel <dharmi.patel.cg@gmail.com>
2026-05-27 10:56:17 +0530

    Time: 0 ms (100%), Space: 8.4 MB (83.21%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+string build(string str){
+
+        
+        string res = "";
+        for(char ch : str){
+
+            if(ch != '#'){
+                res +=  ch;
+            }
+            else if(!res.empty()){
+                res.pop_back();
+            }
+        }
+        return res;
+}
+
+        bool backspaceCompare(string s, string t) {
+            return build(s) == build(t);
+    }
+};
