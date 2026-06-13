c6b0a91df7f89fe528b2d9b672d7dd451cae2738
Dharmi Patel <dharmi.patel.cg@gmail.com>
2026-06-13 10:23:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (95.37%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -2,24 +2,15 @@ class Solution {
 public:
     int digitFrequencyScore(int n) {
 
-        vector<int> freq(10, 0);
+        int ans = 0;
 
         while(n > 0) {
 
-            int digit = n % 10;
-
-            freq[digit]++;
+            ans += n % 10;
 
             n /= 10;
         }
 
-        int score = 0;
-
-        for(int i = 0; i < 10; i++) {
-
-            score += i * freq[i];
-        }
-
-        return score;
+        return ans;
     }
 };
